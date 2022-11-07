77cb454f964e83f9b71e8b5c5ca358eb51b52dab
eadbeel94 <eadbeel94@outlook.com>
2022-11-07 17:26:31 -0600

    fix: add breakpoints for prefooterimage block

--- components/blocks/pre_footer_image/pre_footer_image.tsx
+++ components/blocks/pre_footer_image/pre_footer_image.tsx
@@ -9,20 +9,18 @@ export const typename = 'Set_Replicator_BlockPreFooter'
 const PreFooterImageBlock:FunctionComponent<{ block: PreFooterImageInterface }> = ({ block }) => {
   console.log(block)
   return (
-    <div
-      className={`${styles.root} container`}
-    >
+    <div className={styles.root}>
       <section className={styles.wrapper}>
         <div className={styles.leftSide}>
           <h3>H3 headline that is titlecase.</h3>
           <p>Get the latest insights from the best in the industry. Every week we provide new content to help optimize your results.</p>
           <section className={styles.groupButtons}>
             <ArrowButton text="Try Outreach" href="#" />
-            <UnderlinedButton text="Explore pricing" href="" underlineColor="#5951FF" />
+            <UnderlinedButton text="Explore pricing" href="#" underlineColor="#5951FF" />
           </section>
         </div>
         <div className={styles.rightSide}>
-          <img src={block.right_image?.src} alt="placeholderimage" />
+          <img className={styles.image} src={block.right_image?.src} alt="placeholderimage" />
         </div>
       </section>
     </div>
