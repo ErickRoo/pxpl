115c042d9d6e088c9afaa667e72674e531ac1779
eadbeel94 <eadbeel94@outlook.com>
2022-11-07 17:01:08 -0600

    fix: return to normal state

--- pages/components.tsx
+++ pages/components.tsx
@@ -1,12 +1,8 @@
 import React, { FC } from 'react'
 import ComponentCatalogueBlock from '../components/blocks/component_catalogue/component_catalogue'
-import PreFooter from './components/PreFooter'
 
 const components: FC = () => (
-  <>
-    <ComponentCatalogueBlock block={{}} />
-    <PreFooter />
-  </>
+  <ComponentCatalogueBlock block={{}} />
 )
 
 export default components
