1ddb598dedc0c6309e77a2df28e9a31e2a757d85
eadbeel94 <eadbeel94@outlook.com>
2022-11-08 09:36:24 -0600

    fix: reorder styles using OneColumnBody component

--- components/blocks/pre_footer/pre_footer.tsx
+++ components/blocks/pre_footer/pre_footer.tsx
@@ -1,60 +1,58 @@
 import React, { FunctionComponent } from 'react'
-import ArrowButton from 'components/generic/arrow-button/arrow-button'
-import UnderlinedButton from 'components/generic/underlined-button/underlined-button'
+import OneColumnBody from 'components/generic/one-column-body/one-column-body'
 import styles from './pre_footer.module.scss'
 import PreFooterImageInterface from './pre_footer.interface'
 
 export const typename = 'Set_Replicator_BlockPreFooter'
 
 const PreFooterImageBlock:FunctionComponent<{ block: PreFooterImageInterface }> = ({ block }) => {
-  if (block?.variant === 'image') {
-    return (
-      <div className={styles.root}>
+  if (!block?.variant) {
+    return null
+  }
+
+  const renderBody = () => {
+    if (block.variant === 'image') {
+      return (
         <section className={`${styles.wrapper} ${styles.wrapper__variantImage}`}>
-          <div className={styles.leftSide}>
-            <h3>{block.headlineLeft}</h3>
-            <p>{block.description}</p>
-            <section className={styles.groupButtons}>
-              {block?.primaryCta && (
-                <ArrowButton text={block.primaryCta.ctaText} href={block.primaryCta.ctaLink} />
-              )}
-              {block?.secondaryCta && (
-                <UnderlinedButton text={block.secondaryCta.ctaText} href={block.secondaryCta.ctaLink} underlineColor="#5951FF" />
-              )}
-            </section>
-          </div>
+          <OneColumnBody
+            className={styles.leftSide}
+            headline={block.headlineLeft}
+            headlineLevel={3}
+            description={block.description}
+            primaryCta={block.primaryCta}
+            secondaryCta={block.secondaryCta}
+          />
           <div className={styles.rightSide}>
             <img className={styles.image} src={block.right_image?.src} alt="placeholderimage" />
           </div>
         </section>
-      </div>
-    )
-  }
-  if (block?.variant === 'headline') {
-    return (
-      <div className={styles.root}>
+      )
+    }
+
+    if (block.variant === 'headline') {
+      return (
         <section className={styles.wrapper}>
           <div className={styles.leftSide}>
             <h2>{block.headlineLeft}</h2>
           </div>
-          <div className={styles.rightSide}>
-            <h4>{block.headlineRight}</h4>
-            <p>{block.description}</p>
-            <section className={styles.groupButtons}>
-              {block?.primaryCta && (
-                <ArrowButton text={block.primaryCta.ctaText} href={block.primaryCta.ctaLink} />
-              )}
-              {block?.secondaryCta && (
-                <UnderlinedButton text={block.secondaryCta.ctaText} href={block.secondaryCta.ctaLink} underlineColor="#5951FF" />
-              )}
-            </section>
-          </div>
+          <OneColumnBody
+            className={styles.rightSide}
+            headline={block.headlineRight}
+            headlineLevel={4}
+            description={block.description}
+            primaryCta={block.primaryCta}
+            secondaryCta={block.secondaryCta}
+          />
         </section>
-      </div>
-    )
+      )
+    }
+
+    return null
   }
 
-  return null
+  return (
+    <div className={styles.root}>{renderBody()}</div>
+  )
 }
 
 export default PreFooterImageBlock
