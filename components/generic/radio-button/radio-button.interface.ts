64a95dbb87c61eede5b6fbb796721f8ddfbafbda
eadbeel94 <eadbeel94@outlook.com>
2022-11-08 11:55:28 -0600

    fix: add radio button

---
+++ components/generic/radio-button/radio-button.interface.ts
@@ -0,0 +1,4 @@
+export interface RadioButtonInterface {
+  className?: string;
+  id: string;
+}
---
+++ components/generic/radio-button/radio-button.tsx
@@ -0,0 +1,20 @@
+import React, { useState, FC } from 'react'
+// import Link from 'next/link'
+import { RadioButtonInterface } from './radio-button.interface'
+import styles from './radio-button.module.scss'
+import Check from '../../../assets/RadioButton/check.svg'
+
+const RadioButton: FC<RadioButtonInterface> = ({ className = '', id }) => {
+  const [selected, setSelected] = useState(false)
+
+  return (
+    <div className={`${styles.root} ${className}`}>
+      <label htmlFor={id}>
+        <Check className={styles.icon} data-checked={selected} />
+        <input className={styles.input} id={id} type="radio" checked={selected} onClick={() => setSelected(!selected)} />
+      </label>
+    </div>
+  )
+}
+
+export default RadioButton
