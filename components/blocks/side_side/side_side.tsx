4cc8676f7c696d4dba8de211cb2e8bc5c33053b7
eadbeel94 <eadbeel94@outlook.com>
2022-11-09 12:40:37 -0600

    fix: toggle show circles

--- components/blocks/side_side/side_side.tsx
+++ components/blocks/side_side/side_side.tsx
@@ -7,8 +7,15 @@ export const typename = 'Set_Replicator_BlockSideSide'
 
 const SideSideBlock:FunctionComponent<{ block: SideSideInterface }> = ({ block }) => (
   <div className={styles.root}>
-    <section className={styles.wrapper}>
+    <section className={`${styles.wrapper} ${block.reverse ? `${styles.wrapper__reverse}` : ''}`}>
       <div className={styles.leftSide}>
+        {block.showCircles && (
+          <ul className={styles.circlesContainer}>
+            <li className={styles.circleLeft} />
+            <li className={styles.circleCenter} />
+            <li className={styles.circleRight} />
+          </ul>
+        )}
         <img className={styles.image} src={block.image?.src} alt="placeholderimage" />
       </div>
       <OneColumnBody
--- pages/components/SideSide.tsx
+++ pages/components/SideSide.tsx
@@ -3,7 +3,7 @@ import SideSideInterface from 'components/blocks/side_side/side_side.interface'
 import SideSideBlock from 'components/blocks/side_side/side_side'
 import PlaceholderImage from '../../assets/Sideside/placeholder.png'
 
-const placeholderSideSideBlock = {
+const placeholderFirstSideSideBlock = {
   topTag: 'Eyebrow sentence case',
   heading: 'H2 headline that can be up to four lines or less ipsum.',
   description: 'AI and Machine Learning are at the core of everything Outreach does for your Sales Leaders.',
@@ -12,12 +12,29 @@ const placeholderSideSideBlock = {
     ctaText: 'Learn more',
     ctaLink: '#',
   },
+  showCircles: true,
 } as unknown as SideSideInterface
+
+/*
+const placeholderSecondSideSideBlock = {
+  topTag: 'Eyebrow sentence case',
+  reverse: true,
+  heading: 'H2 headline that can be up to four lines or less ipsum.',
+  description: 'AI and Machine Learning are at the core of everything Outreach does for your Sales Leaders.',
+  image: PlaceholderImage,
+  primaryCta: {
+    ctaText: 'Learn more',
+    ctaLink: '#',
+  },
+} as unknown as SideSideInterface
+*/
 
 const SideSide: FC = () => (
   <div style={{ backgroundColor: '#eee' }}>
-    <SideSideBlock block={placeholderSideSideBlock} />
-    <div style={{ margin: '3rem' }} />
+    <div style={{ padding: '3rem', backgroundColor: '#fff' }} />
+    <SideSideBlock block={placeholderFirstSideSideBlock} />
+    <div style={{ padding: '3rem', backgroundColor: '#fff' }} />
+    <div style={{ padding: '3rem', backgroundColor: '#fff' }} />
   </div>
 )
 
