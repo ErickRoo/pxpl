36cf407bd3a1edb72fc43429581fad95606017fc
eadbeel94 <eadbeel94@outlook.com>
2022-11-09 16:32:09 -0600

    fix: set correct placeholder-b and positions

--- components/blocks/side_side/side_side.tsx
+++ components/blocks/side_side/side_side.tsx
@@ -9,14 +9,20 @@ const SideSideBlock:FunctionComponent<{ block: SideSideInterface }> = ({ block }
   <div className={styles.root}>
     <section className={`${styles.wrapper} ${block.reverse ? `${styles.wrapper__reverse}` : ''}`}>
       <div className={`${styles.wrapperImage} ${block.reverse ? `${styles.wrapperImage__reverse}` : ''}`}>
-        {block.showCircles && (
-          <ul className={`${styles.circlesContainer} ${block.reverse ? `${styles.circlesContainer__reverse}` : ''}`}>
-            <li />
-            <li />
-            <li />
-          </ul>
+        {block.showCircles ? (
+          <>
+            <ul className={`${styles.circlesContainer} ${block.reverse ? `${styles.circlesContainer__reverse}` : ''}`}>
+              <li />
+              <li />
+              <li />
+            </ul>
+            <img src={block.image?.src} alt="placeholderimage" />
+          </>
+        ) : (
+          <section className={`${styles.bgImage} ${block.reverse ? `${styles.bgImage__reverse}` : ''}`}>
+            <div style={{ backgroundImage: `url(${block.image?.src})` }} />
+          </section>
         )}
-        <img className={styles.image} src={block.image?.src} alt="placeholderimage" />
       </div>
       <div className={`${styles.wrapperContent} ${block.reverse ? `${styles.wrapperContent__reverse}` : ''}`}>
         <OneColumnBody
