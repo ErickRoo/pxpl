1382f99fbcf5037f72154f6a3176f3c27f4bc1b9
eadbeel94 <eadbeel94@outlook.com>
2022-11-09 13:40:04 -0600

    fix: add placeholder

--- pages/components/SideSide.tsx
+++ pages/components/SideSide.tsx
@@ -1,13 +1,14 @@
 import { FC } from 'react'
 import SideSideInterface from 'components/blocks/side_side/side_side.interface'
 import SideSideBlock from 'components/blocks/side_side/side_side'
-import PlaceholderImage from '../../assets/Sideside/placeholder.png'
+import PlaceholderAImage from '../../assets/Sideside/placeholder-a.png'
+import PlaceholderBImage from '../../assets/Sideside/placeholder-b.png'
 
 const placeholderFirstSideSideBlock = {
   topTag: 'Eyebrow sentence case',
   heading: 'H2 headline that can be up to four lines or less ipsum.',
   description: 'AI and Machine Learning are at the core of everything Outreach does for your Sales Leaders.',
-  image: PlaceholderImage,
+  image: PlaceholderAImage,
   primaryCta: {
     ctaText: 'Learn more',
     ctaLink: '#',
@@ -20,12 +21,35 @@ const placeholderSecondSideSideBlock = {
   reverse: true,
   heading: 'H2 headline that can be up to four lines or less ipsum.',
   description: 'AI and Machine Learning are at the core of everything Outreach does for your Sales Leaders.',
-  image: PlaceholderImage,
+  image: PlaceholderAImage,
   primaryCta: {
     ctaText: 'Learn more',
     ctaLink: '#',
   },
   showCircles: true,
+} as unknown as SideSideInterface
+
+const placeholderThirdSideSideBlock = {
+  topTag: 'Eyebrow sentence case',
+  heading: 'H2 headline that can be up to four lines or less ipsum.',
+  description: 'AI and Machine Learning are at the core of everything Outreach does for your Sales Leaders.',
+  image: PlaceholderBImage,
+  primaryCta: {
+    ctaText: 'Learn more',
+    ctaLink: '#',
+  },
+} as unknown as SideSideInterface
+
+const placeholderFourthSideSideBlock = {
+  topTag: 'Eyebrow sentence case',
+  reverse: true,
+  heading: 'H2 headline that can be up to four lines or less ipsum.',
+  description: 'AI and Machine Learning are at the core of everything Outreach does for your Sales Leaders.',
+  image: PlaceholderBImage,
+  primaryCta: {
+    ctaText: 'Learn more',
+    ctaLink: '#',
+  },
 } as unknown as SideSideInterface
 
 const SideSide: FC = () => (
@@ -34,6 +58,10 @@ const SideSide: FC = () => (
     <SideSideBlock block={placeholderFirstSideSideBlock} />
     <div style={{ padding: '3rem', backgroundColor: '#fff' }} />
     <SideSideBlock block={placeholderSecondSideSideBlock} />
+    <div style={{ padding: '3rem', backgroundColor: '#fff' }} />
+    <SideSideBlock block={placeholderThirdSideSideBlock} />
+    <div style={{ padding: '3rem', backgroundColor: '#fff' }} />
+    <SideSideBlock block={placeholderFourthSideSideBlock} />
     <div style={{ padding: '3rem', backgroundColor: '#fff' }} />
   </div>
 )
