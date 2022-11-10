b45befa6bb6e168e57504d650ab0ae0f43ea6a82
eadbeel94 <eadbeel94@outlook.com>
2022-11-10 13:51:20 -0600

    fix: set only side side url

--- components/blocks/component_catalogue/component_catalogue.tsx
+++ components/blocks/component_catalogue/component_catalogue.tsx
@@ -12,9 +12,6 @@ const ComponentCatalogueBlock: FunctionComponent<{ block: ComponentCatalogueInte
       <Link href="/components/ContentStack">
         <p style={{ fontSize: 20, marginBottom: 20 }}>Content Stack</p>
       </Link>
-      <Link href="/components/PreFooter">
-        <p style={{ fontSize: 20, marginBottom: 20 }}>PreFooter</p>
-      </Link>
       <Link href="/components/SideSide">
         <p style={{ fontSize: 20, marginBottom: 20 }}>SideSide</p>
       </Link>
