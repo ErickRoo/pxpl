7d405cef29c9ea7080925597aaeb05eb02b1c679
eadbeel94 <eadbeel94@outlook.com>
2022-11-18 13:39:55 -0600

    fix: set size for mobile version

--- components/blocks/stats/stats.tsx
+++ components/blocks/stats/stats.tsx
@@ -22,20 +22,20 @@ const StatsBlock:FunctionComponent<{ block: StatsInterface }> = ({ block }) => {
               <ArrowCorner className={styles.arrowCorner} />
               <div className={styles.wrapper}>
                 <div className={styles.row}>
-                  <div className={styles.leftSide}>
+                  <div className={`${styles.leftSide} pt-[2.4rem] md:pt-[2.2rem]`}>
                     <p className="eyebrow">{top.headline}</p>
                     <h6 className="typo-h6">{top.description}</h6>
                   </div>
-                  <div className="typo-stats">
+                  <div className={`typo-stats ${styles.rightSide}`}>
                     {top.stat}
                   </div>
                 </div>
                 <div className={styles.row}>
-                  <div className={styles.leftSide}>
+                  <div className={`${styles.leftSide} pt-[2.4rem] md:pt-[2.6rem]`}>
                     <p className="eyebrow">{bottom.headline}</p>
                     <h6 className="typo-h6">{bottom.description}</h6>
                   </div>
-                  <div className="typo-stats">
+                  <div className={`typo-stats ${styles.rightSide}`}>
                     {bottom.stat}
                   </div>
                 </div>
