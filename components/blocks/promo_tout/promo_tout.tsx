8212740f4b03354711d9a35c763014f5fd761701
eadbeel94 <eadbeel94@outlook.com>
2022-11-19 12:45:48 -0600

    fix: set styles for variant signup in desktop view
    fix: add variant to tertiary button
    
    fix: switch between variants

--- components/blocks/promo_tout/promo_tout.tsx
+++ components/blocks/promo_tout/promo_tout.tsx
@@ -1,15 +1,43 @@
 import React, { FunctionComponent } from 'react'
-import styles from './promo_tout.module.scss'
+
+import FormNewsletter from 'components/generic/form-newsletter/form-newsletter'
+import TertiaryButton from 'components/generic/tertiary-button/tertiary-button'
 import PromoToutInterface from './promo_tout.interface'
+import styles from './promo_tout.module.scss'
 
 export const typename = 'Set_Replicator_BlockPromoTout'
 
 const PromoToutBlock:FunctionComponent<{ block: PromoToutInterface }> = ({ block }) => (
   <div>
     {block.TextAndImageBlocks?.map((oneTextImageItem, idx) => {
-      const key = `side-side-item-${idx}`
+      const key = `promo-tout-item-${idx}`
+
+      if (oneTextImageItem.variant === 'image') {
+        return (
+          <div className={styles.root} key={key}>
+            image
+          </div>
+        )
+      }
+
+      const wrapperStyles = `${styles.wrapper} ${oneTextImageItem?.centered ? styles.wrapper__centered : ''}`
       return (
-        <div className={styles.root} key={key}>something</div>
+        <div className={styles.root} key={key}>
+          <section className={wrapperStyles}>
+            <div className={styles.leftSide}>
+              <h5 className={`typo-h5 ${styles.headline}`}>{oneTextImageItem.headline}</h5>
+              <p className="typo-body">{oneTextImageItem.description}</p>
+            </div>
+            <div className={styles.rightSide}>
+              {oneTextImageItem.variant === 'signup' && <FormNewsletter className={styles.wrapperForm} />}
+              {oneTextImageItem.variant === 'cta' && (
+                <div className={styles.wrapperButton}>
+                  <TertiaryButton type="link" text="Learn more" variant="outline" href={oneTextImageItem.ctaLink} />
+                </div>
+              )}
+            </div>
+          </section>
+        </div>
       )
     })}
   </div>
--- pages/components/PromoTout.tsx
+++ pages/components/PromoTout.tsx
@@ -4,11 +4,33 @@ import PromoToutInterface from 'components/blocks/promo_tout/promo_tout.interfac
 import PromoToutBlock from 'components/blocks/promo_tout/promo_tout'
 import PlaceholderImage from '../../assets/PromoTout/placeholder.png'
 
-const placeholderPromoToutBlock = {
+const placeholderPromoToutABlock = {
   variant: 'signup',
   headline: 'Stay up-to-date with all things Outreach.',
   description: 'Get the latest product news, industry insights, and valueable resources in your inbox.',
   image: PlaceholderImage,
+}
+
+const placeholderPromoToutBBlock = {
+  variant: 'signup',
+  headline: 'Stay up-to-date with all things Outreach.',
+  description: 'Get the latest product news, industry insights, and valueable resources in your inbox.',
+  centered: true,
+}
+
+const placeholderPromoToutCBlock = {
+  variant: 'cta',
+  headline: 'Lorem ipsum dolor sit amet.',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+  ctaLink: '#',
+}
+
+const placeholderPromoToutDBlock = {
+  variant: 'cta',
+  headline: 'Lorem ipsum dolor sit amet.',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+  ctaLink: '#',
+  centered: true,
 }
 
 const PromoTout: FC = () => (
@@ -16,7 +38,10 @@ const PromoTout: FC = () => (
     block={{
       type: 'block',
       TextAndImageBlocks: [
-        placeholderPromoToutBlock,
+        placeholderPromoToutABlock,
+        placeholderPromoToutBBlock,
+        placeholderPromoToutCBlock,
+        placeholderPromoToutDBlock,
       ],
     } as unknown as PromoToutInterface}
   />
