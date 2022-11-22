5f9d0423689c8e1903e2f6f18de182873423ba30
eadbeel94 <eadbeel94@outlook.com>
2022-11-22 11:43:28 -0600

    fix: set correct text for each breakpoint
    fix: set spaces on large desktop view
    
    fix: set spaces on tablet view
    
    fix: resize spaces

--- pages/components/HeadlineText.tsx
+++ pages/components/HeadlineText.tsx
@@ -1,88 +1,88 @@
 import React, { FC } from 'react'
+
+import getCurrentBreakpoint from 'utils/breakpoints/getCurrentBreakpoint'
+import useWindowSize from 'styles/getBreakpointQuery'
 import HeadlineTextInterface from 'components/blocks/headline_text/headline_text.interface'
 import HeadlineTextBlock from 'components/blocks/headline_text/headline_text'
 
+const getHeadlinePlaceHolder = ({
+  centered = false, bgLink = false, alternativeA = false, alternativeB = false,
+}) => {
+  let headline = 'One platform to rule them all.'
+  if (alternativeA) headline = 'H3 headline multi line'
+  else if (alternativeB) headline = 'H2 headline single line'
+
+  return {
+    type: 'Set_Replicator_BlockHeadlineText',
+    variant: centered ? 'centerText' : 'leftText',
+    eyebrow: (alternativeA || alternativeB) ? 'Eyebrow' : 'The Outreach Sales Execution Platform',
+    headline,
+    description: `Give revenue leaders an unfair advantage through industry-leading workflows,
+      AI-generated insights, and freakishly accurate forcasting.`,
+    primaryCta: {
+      ctaText: 'Learn more',
+      ctaLink: '#',
+    },
+    secondaryCta: {
+      ctaText: 'Explore pricing',
+      ctaLink: '#',
+    },
+    background_image: bgLink ? '/images/headline-text-bg-default.png' : '',
+  } as HeadlineTextInterface
+}
+
 const placeholderCenterText = {
-  type: 'Set_Replicator_BlockHeadlineText',
-  variant: 'centerText',
-  eyebrow: 'The Outreach Sales Execution Platform',
-  headline: 'One platform to rule them all.',
-  description: `Give revenue leaders an unfair advantage through industry-leading workflows,
-    AI-generated insights, and freakishly accurate forcasting.`,
-  primaryCta: {
-    ctaText: 'Learn more',
-    ctaLink: '#',
-  },
-  secondaryCta: {
-    ctaText: 'Explore pricing',
-    ctaLink: '#',
-  },
-} as HeadlineTextInterface
+  normal: getHeadlinePlaceHolder({ centered: true, alternativeB: true }),
+  sm: getHeadlinePlaceHolder({ centered: true, alternativeB: true }),
+  md: getHeadlinePlaceHolder({ centered: true }),
+  lg: getHeadlinePlaceHolder({ centered: true }),
+  xl: getHeadlinePlaceHolder({ centered: true, alternativeB: true }),
+  xxl: getHeadlinePlaceHolder({ centered: true, alternativeB: true }),
+}
 
 const placeholderCenterTextBgImage = {
-  type: 'Set_Replicator_BlockHeadlineText',
-  variant: 'centerText',
-  eyebrow: 'The Outreach Sales Execution Platform',
-  headline: 'One platform to rule them all.',
-  description: `Give revenue leaders an unfair advantage through industry-leading workflows,
-    AI-generated insights, and freakishly accurate forcasting.`,
-  primaryCta: {
-    ctaText: 'Learn more',
-    ctaLink: '#',
-  },
-  secondaryCta: {
-    ctaText: 'Explore pricing',
-    ctaLink: '#',
-  },
-  background_image: '/images/headline-text-bg-default.png',
-} as HeadlineTextInterface
+  normal: getHeadlinePlaceHolder({ centered: true, alternativeA: true, bgLink: true }),
+  sm: getHeadlinePlaceHolder({ centered: true, alternativeB: true, bgLink: true }),
+  md: getHeadlinePlaceHolder({ centered: true, bgLink: true }),
+  lg: getHeadlinePlaceHolder({ centered: true, bgLink: true }),
+  xl: getHeadlinePlaceHolder({ centered: true, alternativeB: true, bgLink: true }),
+  xxl: getHeadlinePlaceHolder({ centered: true, alternativeB: true, bgLink: true }),
+}
 
 const placeholderLeftText = {
-  type: 'Set_Replicator_BlockHeadlineText',
-  variant: 'leftText',
-  eyebrow: 'The Outreach Sales Execution Platform',
-  headline: 'One platform to rule them all.',
-  description: `Give revenue leaders an unfair advantage through industry-leading workflows,
-    AI-generated insights, and freakishly accurate forcasting.`,
-  primaryCta: {
-    ctaText: 'Learn more',
-    ctaLink: '#',
-  },
-  secondaryCta: {
-    ctaText: 'Explore pricing',
-    ctaLink: '#',
-  },
-} as HeadlineTextInterface
+  normal: getHeadlinePlaceHolder({ alternativeA: true }),
+  sm: getHeadlinePlaceHolder({ alternativeB: true }),
+  md: getHeadlinePlaceHolder({}),
+  lg: getHeadlinePlaceHolder({}),
+  xl: getHeadlinePlaceHolder({ alternativeB: true }),
+  xxl: getHeadlinePlaceHolder({ alternativeB: true }),
+}
 
 const placeholderLeftTextBgImage = {
-  type: 'Set_Replicator_BlockHeadlineText',
-  variant: 'leftText',
-  eyebrow: 'The Outreach Sales Execution Platform',
-  headline: 'One platform to rule them all.',
-  description: `Give revenue leaders an unfair advantage through industry-leading workflows,
-    AI-generated insights, and freakishly accurate forcasting.`,
-  primaryCta: {
-    ctaText: 'Learn more',
-    ctaLink: '#',
-  },
-  secondaryCta: {
-    ctaText: 'Explore pricing',
-    ctaLink: '#',
-  },
-  background_image: '/images/headline-text-bg-default.png',
-} as HeadlineTextInterface
+  normal: getHeadlinePlaceHolder({ alternativeA: true, bgLink: true }),
+  sm: getHeadlinePlaceHolder({ alternativeB: true, bgLink: true }),
+  md: getHeadlinePlaceHolder({ bgLink: true }),
+  lg: getHeadlinePlaceHolder({ bgLink: true }),
+  xl: getHeadlinePlaceHolder({ alternativeB: true, bgLink: true }),
+  xxl: getHeadlinePlaceHolder({ alternativeB: true, bgLink: true }),
+}
 
-const HeadlineText: FC = () => (
-  <>
-    <HeadlineTextBlock block={placeholderCenterText} />
-    <div style={{ padding: '2rem' }} />
-    <HeadlineTextBlock block={placeholderLeftText} />
-    <div style={{ padding: '2rem' }} />
-    <HeadlineTextBlock block={placeholderLeftTextBgImage} />
-    <div style={{ padding: '2rem' }} />
-    <HeadlineTextBlock block={placeholderCenterTextBgImage} />
-    <div style={{ padding: '2rem' }} />
-  </>
-)
+const HeadlineText: FC = () => {
+  const { width } = useWindowSize()
+  const { position = '' } = getCurrentBreakpoint(width) ?? {}
+
+  return (
+    <>
+      <HeadlineTextBlock block={placeholderCenterText[position]} />
+      <div style={{ padding: '2rem' }} />
+      <HeadlineTextBlock block={placeholderLeftText[position]} />
+      <div style={{ padding: '2rem' }} />
+      <HeadlineTextBlock block={placeholderLeftTextBgImage[position]} />
+      <div style={{ padding: '2rem' }} />
+      <HeadlineTextBlock block={placeholderCenterTextBgImage[position]} />
+      <div style={{ padding: '2rem' }} />
+    </>
+  )
+}
 
 export default HeadlineText
