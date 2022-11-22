93fade1b211dd5f5cc6b70da4834624033d8e536
eadbeel94 <eadbeel94@outlook.com>
2022-11-22 13:13:41 -0600

    fix: add headline to catalogue

--- components/blocks/component_catalogue/component_catalogue.tsx
+++ components/blocks/component_catalogue/component_catalogue.tsx
@@ -24,6 +24,9 @@ const ComponentCatalogueBlock: FunctionComponent<{ block: ComponentCatalogueInte
       <Link href="/components/LargeQuote">
         <p style={{ fontSize: 20, marginBottom: 20 }}>LargeQuote</p>
       </Link>
+      <Link href="/components/HeadlineText">
+        <p style={{ fontSize: 20, marginBottom: 20 }}>HeadlineText</p>
+      </Link>
     </div>
   </div>
 )
