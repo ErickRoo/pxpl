53db9f2b7cd7acb8b3fe2cd0c8ebd5f03d5785c0
eadbeel94 <eadbeel94@outlook.com>
2022-11-23 12:51:00 -0600

    fix: add styles for card outline in desktop version
    fix: render cards based on placeholder json
    
    fix: add rest variants
    
    fix: set space in desktop view
    
    fix: reorder placeholder based on breakpoints

--- components/blocks/content_card/content_card.tsx
+++ components/blocks/content_card/content_card.tsx
@@ -1,13 +1,42 @@
 import React, { FunctionComponent } from 'react'
 
+import CardOutlineItem from 'components/generic/card_outline_item/card_outline_item'
 import ContentCardInterface from './content_card.interface'
 import styles from './content_card.module.scss'
 
 export const typename = 'Set_Replicator_BlockContentCard'
 
-const ContentCardBlock:FunctionComponent<{ block: ContentCardInterface }> = () => (
+const ContentCardBlock:FunctionComponent<{ block: ContentCardInterface }> = ({ block }) => (
   <div className={styles.root}>
-    ContentCard Block
+    <section className={styles.wrapper}>
+      {block?.rows?.map((oneRow, idx) => {
+        const key = `one-row-${idx}`
+
+        return (
+          <div key={key} className={`${styles.row} ${styles?.[`row__variant${oneRow.quantity}`]}`}>
+            {oneRow.items.map((oneItem, index) => {
+              const subkey = `one-item-${index}`
+
+              return (
+                <CardOutlineItem
+                  key={subkey}
+                  variant={`variant${oneRow.quantity}`}
+                  category={oneItem.category}
+                  image={oneItem.image}
+                  icon={oneItem.icon}
+                  eyebrow={oneItem.eyebrow}
+                  date={oneItem.date}
+                  headline={oneItem.headline}
+                  description={oneItem.description}
+                  ctaLink={oneItem.ctaLink}
+                  tags={oneItem.tags}
+                />
+              )
+            })}
+          </div>
+        )
+      })}
+    </section>
   </div>
 )
 
